01d3845cd9c433f30e49f667068a7c919f65734d
Dylan Sprague <dsprague@docuphase.com>
2019-03-29 16:36:39 -0400

    Add useNextVariants to theme to avoid Typography deprecation warnings

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ const theme = createMuiTheme({
                 fontFamily: "'Uncial Antiqua', cursive"
             }
         }
+    },
+    typography: {
+        useNextVariants: true
     }
 });
 
