5ca3d7c25091c82b9bd61389b701c3689767ada3
Dylan Sprague <dsprague@docuphase.com>
2019-03-29 15:51:14 -0400

    Add borders to upgrade list items

--- src/components/UpgradeDisplay.tsx
+++ src/components/UpgradeDisplay.tsx
@@ -2,6 +2,7 @@ import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import { FunctionComponent } from "react";
 import { FoodUpgrade, foodUpgrades } from "../game/FoodUpgrades";
@@ -17,18 +18,46 @@ export const UpgradeDisplay: FunctionComponent<UpgradeDisplayProps> = (props) =>
     return (
         <List>
             {availableUpgrades.map((upgrade) => (
-                <ListItem>
+                <ListItem style={{borderBottom: "1px solid gray"}}>
                     <Tooltip title={upgrade.description} placement="right">
                         <Button
                             variant="outlined"
                             onClick={() => props.purchaseFoodUpgrade(upgrade)}
                             disabled={props.purchasedFoodUpgrades.includes(upgrade)}
+                            style={{
+                                whiteSpace: "nowrap",
+                                paddingLeft: "1.5rem",
+                                paddingRight: "1.5rem"
+                            }}
                         >
                             {upgrade.name}
                         </Button>
                     </Tooltip>
+                    <div style={{width: "inherit", textAlign: "right"}}>
+                        <Typography>
+                            {displayCost(upgrade)}
+                        </Typography>
+                    </div>
                 </ListItem>
             ))}
         </List>
     );
+};
+
+const displayCost = (upgrade: FoodUpgrade): string => {
+    const costText: string[] = [];
+
+    if (upgrade.cost.food > 0) {
+        costText.push(`${upgrade.cost.food} food`);
+    }
+
+    if (upgrade.cost.wood > 0) {
+        costText.push(`${upgrade.cost.wood} wood`);
+    }
+
+    if (upgrade.cost.gold > 0) {
+        costText.push(`${upgrade.cost.gold} gold`);
+    }
+
+    return costText.join(", ");
 };
