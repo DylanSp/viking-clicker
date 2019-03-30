9a4bbb5b133094ca9947a0bee8f1d4f5b2f88fa9
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-03-30 18:02:46 -0400

    Add MessageDisplay component to MainPanel

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,9 +1,9 @@
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import { FunctionComponent } from "react";
 import { Resources } from "../game/Resources";
 import { Actions } from "./Actions";
+import { MessageDisplay } from "./MessageDisplay";
 import { ResourceDisplay } from "./ResourceDisplay";
 import { StatusDescription } from "./StatusDescription";
 
@@ -27,6 +27,7 @@ export const MainPanel: FunctionComponent<MainPanelProps> = (props) => {
             <StatusDescription />
             <Actions handlePlow={props.handlePlow} handleChop={props.handleChop} />
             <ResourceDisplay resources={props.resources} />
+            <MessageDisplay />
         </div>
     );
 };
