711aca4ca85b4859ab49de9e12ac36accea1faf4
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-04-07 22:31:00 -0400

    Get prototype of panel fade-in working

--- src/components/ServantsPanel.tsx
+++ src/components/ServantsPanel.tsx
@@ -1,15 +1,41 @@
 import Typography from "@material-ui/core/Typography";
-import * as React from "react";
-import { FunctionComponent } from "react";
+import React, { Component } from "react";
+import { CSSTransition } from "react-transition-group";
 import { ServantsDisplay } from "./ServantsDisplay";
+import "./ServantsPanel.css";
 
-export const ServantsPanel: FunctionComponent = () => {
-    return (
-        <div style={{textAlign: "center"}}>
-            <Typography variant="h4">
-                Servants
-            </Typography>
-            <ServantsDisplay />
-        </div>
-    );
-};
+interface ServantsPanelState {
+    isVisible: boolean;
+}
+
+export class ServantsPanel extends Component<{}, ServantsPanelState> {
+    public constructor(props: {}) {
+        super(props);
+        this.state = {
+            isVisible: false
+        };
+    }
+
+    // button only exists for testing
+    public render() {
+        return (
+            <>
+                <button onClick={this.transitionIn}>test</button>
+                <CSSTransition in={this.state.isVisible} timeout={1000} unmountOnExit={true} classNames="servant-panel">
+                    <div style={{textAlign: "center"}}>
+                        <Typography variant="h4">
+                            Servants
+                        </Typography>
+                        <ServantsDisplay />
+                    </div>
+                </CSSTransition>
+            </>
+        );
+    }
+
+    private transitionIn = () => {
+        this.setState({
+            isVisible: true
+        });
+    }
+}
